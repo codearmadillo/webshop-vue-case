6d0c4941e2ca276b8eb92382069dad09dd6135dd
jiri-kralovec-eal <jiri0042@edu.eal.dk>
2019-02-16 18:56:47 +0100

    Move logic for fetching categories into separate method

--- src/main.js
+++ src/main.js
@@ -1,62 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
-import ProductListView from './views/view.product-list.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import ProductListView from "./views/view.product-list.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const Application = new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
   render: h => h(App),
-  beforeCreate() {
+  created() {
 
-    /*
-    Fetch product categories
-    Create routing options
-    */
-    axios.get('/data/data.product-categories.json').then(response => {
-
-      let Routes = [];
-
-      function TreeToObjects (tree, path, breadcrumb) {
-
-        tree.forEach(element => {
-
-          let bread = [{
-            path: path + element.slug,
-            title: element.title
-          }];
-
-          let breadcrumbs = breadcrumb.concat(bread);
-
-          Routes.push({
-            path: path + element.slug,
-            meta: {
-              title: element.title,
-              breadcrumbs: breadcrumbs,
-              catId: element.id,
-              hasLink: element.hasLink,
-              hasChildren: element.hasOwnProperty('children') && element.children.length > 0
-            },
-            component: ProductListView
-          });
-
-          if (element.hasOwnProperty('children')) {
-            TreeToObjects(element.children, path + element.slug + '/', breadcrumbs);
-          }
-
-        });
-      }
-      
-      TreeToObjects(response.data.children, response.data.root, []);
-
-      this.$router.addRoutes(Routes);
-
-    });
+    this.fetchAndRouteCategories();
 
   },
   data: {
@@ -68,30 +26,80 @@ const Application = new Vue({
       },
       favourites: [],
       shop: {
-        vat: 0.20,
-        currency: 'eur'
+        vat: 0.2,
+        currency: "eur"
       }
     },
     currencies: {
       eur: {
-        id: 'eur',
-        tag: 'EUR',
-        symbol: '€',
+        id: "eur",
+        tag: "EUR",
+        symbol: "€",
         value: 1,
         default: true
       },
-      usd:  {
-        id: 'usd',
-        tag: 'USD',
-        symbol: '$',
+      usd: {
+        id: "usd",
+        tag: "USD",
+        symbol: "$",
         value: 1.1298
       },
       gbp: {
-        id: 'gbp',
-        tag: 'GBP',
-        symbol: '£',
+        id: "gbp",
+        tag: "GBP",
+        symbol: "£",
         value: 0.8764
       }
+    }
+  },
+  methods: {
+    fetchAndRouteCategories() {
+      return axios.get("/data/data.product-categories.json").then(response => {
+
+        let Routes = [];
+
+        function ParseTree(tree, path, breadcrumb) {
+          tree.forEach(element => {
+
+            let Bread = [
+              {
+                path: path + element.slug,
+                title: element.title
+              }
+            ];
+            
+            let Breadcrumbs = breadcrumb.concat(Bread);
+
+            Routes.push({
+              path: path + element.slug,
+              meta: {
+                title: element.title,
+                breadcrumbs: Breadcrumbs,
+                catId: element.id,
+                hasLink: element.hasLink,
+                hasChildren:
+                  element.hasOwnProperty("children") &&
+                  element.children.length > 0
+              },
+              component: ProductListView
+            });
+
+            if(element.hasOwnProperty("children")) {
+              ParseTree(
+                element.children,
+                path + element.slug + "/",
+                Breadcrumbs
+              );
+            }
+
+          });
+        }
+
+        ParseTree(response.data.children, response.data.root, []);
+
+        this.$router.addRoutes(Routes);
+
+      });
     }
   }
 });
