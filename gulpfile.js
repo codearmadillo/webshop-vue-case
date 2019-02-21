b694773c83dcc482e0a633a94ed803e071b54df4
jiri-kralovec-eal <jiri0042@edu.eal.dk>
2019-02-21 13:08:20 +0100

    Gulp workflow no longer supports JS as it was not needed

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,18 @@
 const gulp = require('gulp'),
         sass = require('gulp-sass'),
-        concat = require('gulp-concat'),
-        uglify = require('gulp-uglify'),
         gutil = require('gulp-util'),
         autoprefixer = require('autoprefixer'),
         sourcemaps = require('gulp-sourcemaps'),
         postcss = require('gulp-postcss');
 
 const dependencies = {
-    javascript: Array(
-        'src/assets/script/application.js',
-        'src/assets/script/**/*.js'
-    ),
     stylesheet: Array(
-        "src/assets/scss/*.scss",
-        "src/assets/scss/**/*.scss"
+        "src/scss/*.scss",
+        "src/scss/**/*.scss"
     )
 }
 const input = {
-    stylesheet: "src/assets/scss/default.scss",
-    javascript: dependencies.javascript
+    stylesheet: "src/scss/default.scss"
 }
 const output = {
     production: 'public/production/',
@@ -27,23 +20,14 @@ const output = {
 }
 
 gulp.task('default', ['watch']);
-gulp.task('watch', ['scss', 'script'], function(){
+gulp.task('watch', ['scss'], function(){
     gulp.watch(dependencies.stylesheet, ['scss']);
-    gulp.watch(dependencies.javascript, ['script']);
 });
 gulp.task('scss', function() {
     return gulp.src(input.stylesheet)
         .pipe(gutil.env.type !== 'production' ? sourcemaps.init() : gutil.noop())
         .pipe(sass({outputStyle: gutil.env.type !== 'production' ? 'nested' : 'compressed'}).on('error', sass.logError))
         .pipe(postcss([autoprefixer]))
-        .pipe(gutil.env.type !== 'production' ? sourcemaps.write() : gutil.noop())
-        .pipe(gutil.env.type !== 'production' ? gulp.dest(output.development) : gulp.dest(output.production));
-});
-gulp.task('script', function(){
-    return gulp.src(input.javascript)
-        .pipe(gutil.env.type !== 'production' ? sourcemaps.init() : gutil.noop())
-        .pipe(concat('application.js'))
-        .pipe(gutil.env.type !== 'production' ? gutil.noop() : uglify().on('error', gutil.log))
         .pipe(gutil.env.type !== 'production' ? sourcemaps.write() : gutil.noop())
         .pipe(gutil.env.type !== 'production' ? gulp.dest(output.development) : gulp.dest(output.production));
 });
--- src/assets/script/application.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-var Settings = {
-    null: true
-}
