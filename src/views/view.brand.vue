f42bd6ed846752f658cbe656d423645251c34fd1
jiri-kralovec-eal <jiri0042@edu.eal.dk>
2019-02-26 00:27:43 +0100

    Fixed navigation styling on homepage; Adds brand page

--- src/views/view.brand.vue
+++ src/views/view.brand.vue
@@ -1,6 +1,57 @@
 <template>
     <div class="shop-content brand__wrapper">
-        Brand
+
+        <article class="brand__element">
+            <picture>
+                <source srcset="/assets/images/brand.img001.fhd.jpg" media="(min-width: 1024px)">
+                <source srcset="/assets/images/brand.img001.hd.jpg" media="(min-width: 720px)">
+                <source srcset="/assets/images/brand.img001.md.jpg" media="(min-width: 420px)">
+                <img class="element__image" src="/assets/images/brand.img001.jpg" alt="About the brand" />
+            </picture>
+        </article>
+
+        <article class="brand__element">
+            <picture>
+                <source srcset="/assets/images/brand.img002.fhd.jpg" media="(min-width: 1024px)">
+                <source srcset="/assets/images/brand.img002.hd.jpg" media="(min-width: 720px)">
+                <source srcset="/assets/images/brand.img002.md.jpg" media="(min-width: 420px)">
+                <img class="element__image" src="/assets/images/brand.img002.jpg" alt="About the brand" />
+            </picture>
+        </article>
+
+        <article class="brand__element brand__element--text">
+            <h2 class="element__title">Our values, vision, and stragety</h2>
+            <h3 class="element__subtitle">Cras maximus venenatis malesuada nulla sagittis elit</h3>
+            <p class="element__body">Cras maximus venenatis malesuada. Nulla sagittis elit felis, ac facilisis quam ornare aliquam. Etiam cursus odio vitae dui dignissim, sed tempus lacus tincidunt et donec sapien velit, rhoncus eu nulla a, molestie tempus mi maecenas sagittis ornare. Pellentesque sapien mi, tincidunt nec magna vitae, volutpat elementum odioni lorem Aliquam tempor massa vitae augue mattis tempor id in ante ut augue erat.</p>
+        </article>
+
+        <article class="brand__element brand__element--text">
+            <h2 class="element__title">hand designed clothing</h2>
+            <h3 class="element__subtitle">established in 2013, avenue fashion Sed dictum elit vel sapien luctus eras</h3>
+            <p class="element__body">Cras maximus venenatis malesuada. Nulla sagittis elit felis, ac facilisis quam ornare aliquam. Etiam cursus odio vitae dui dignissim, sed tempus lacus tincidunt et donec sapien velit, rhoncus eu nulla a, molestie tempus mi maecenas sagittis ornare. Pellentesque sapien mi, tincidunt nec magna vitae, volutpat elementum odioni lorem Aliquam tempor massa vitae augue mattis tempor id in ante ut augue erat.</p>
+        </article>
+
+        <article class="brand__element">
+            <picture>
+                <source srcset="/assets/images/brand.img003.fhd.jpg" media="(min-width: 1024px)">
+                <source srcset="/assets/images/brand.img003.hd.jpg" media="(min-width: 720px)">
+                <source srcset="/assets/images/brand.img003.md.jpg" media="(min-width: 420px)">
+                <img class="element__image" src="/assets/images/brand.img003.jpg" alt="About the brand" />
+            </picture>
+        </article>
+
+        <article class="brand__element brand__element--text">
+            <h2 class="element__title">Ethical trading</h2>
+            <h3 class="element__subtitle">we oversee the working conditions of the people in the supply chain</h3>
+            <p class="element__body">Cras maximus venenatis malesuada. Nulla sagittis elit felis, ac facilisis quam ornare aliquam. Etiam cursus odio vitae dui dignissim, sed tempus lacus tincidunt et donec sapien velit, rhoncus eu nulla a, molestie tempus mi maecenas sagittis ornare. Pellentesque sapien mi, tincidunt nec magna vitae, volutpat elementum odioni lorem Aliquam tempor massa vitae augue mattis tempor id in ante ut augue erat.</p>
+        </article>
+
+        <article class="brand__element">
+            <picture>
+                <img class="element__image" src="/assets/images/brand.img-logos.jpg" alt="About the brand" />
+            </picture>
+        </article>
+
     </div>
 </template>
 
